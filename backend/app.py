ca287cd7ec7905a7d0af38f76c39fc0a4ddabf2a
MoonDropX <minalinskyx@hotmail.com>
2017-09-06 18:49:02 +0900

    Change flask port.

--- backend/app.py
+++ backend/app.py
@@ -4,4 +4,4 @@ from mongoengine import connect
 if __name__ == '__main__':
     connect('aigis')
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=5060, threaded=True, debug=True)
